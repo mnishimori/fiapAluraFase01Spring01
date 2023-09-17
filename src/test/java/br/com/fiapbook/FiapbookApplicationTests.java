c14a9be51ac3a96140a545ca38fa05691ebff87b
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2023-09-17 16:00:38 -0300

    test(application): test of application context loaded.

--- src/test/java/br/com/fiapbook/FiapbookApplicationTests.java
+++ src/test/java/br/com/fiapbook/FiapbookApplicationTests.java
@@ -1,13 +1,26 @@
 package br.com.fiapbook;
 
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class FiapbookApplicationTests {
 
+	private final ApplicationContext applicationContext;
+
+	@Autowired
+	FiapbookApplicationTests(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
+
 	@Test
 	void contextLoads() {
+		var fiapBookApplication = applicationContext.getBean(FiapbookApplication.class);
+		Assertions.assertThat(fiapBookApplication).isNotNull();
 	}
 
 }
