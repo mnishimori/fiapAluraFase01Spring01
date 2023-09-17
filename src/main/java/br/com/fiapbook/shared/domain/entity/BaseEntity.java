86b1c0ce33f91aae925264c957a666a8e1ba54c0
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2023-09-17 20:28:02 -0300

    feat(user): set delete attribute as false by default.

--- src/main/java/br/com/fiapbook/shared/domain/entity/BaseEntity.java
+++ src/main/java/br/com/fiapbook/shared/domain/entity/BaseEntity.java
@@ -1,17 +1,22 @@
 package br.com.fiapbook.shared.domain.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
-import java.io.Serializable;
-import java.time.OffsetDateTime;
-import java.util.UUID;
 
 @MappedSuperclass
 @Data
@@ -26,8 +31,9 @@ public abstract class BaseEntity implements Serializable {
     @EqualsAndHashCode.Include
     protected UUID id;
 
+    @Builder.Default
     @Column(nullable = false)
-    protected Boolean deleted;
+    protected Boolean deleted = false;
 
     @Version
     @Column(nullable = false)
