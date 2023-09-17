5277861eddc6dba5a1b94cdbd10f86a47544966e
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2023-09-17 20:38:20 -0300

    configs.

--- src/test/java/br/com/fiapbook/FiapbookApplicationTests.java
+++ src/test/java/br/com/fiapbook/FiapbookApplicationTests.java
@@ -1,13 +1,17 @@
 package br.com.fiapbook;
 
 
+import br.com.fiapbook.shared.annotation.ApiTest;
+import br.com.fiapbook.shared.annotation.DatabaseTest;
+import br.com.fiapbook.shared.annotation.IntegrationTest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+@ApiTest
+@DatabaseTest
 class FiapbookApplicationTests {
 
 	private final ApplicationContext applicationContext;
