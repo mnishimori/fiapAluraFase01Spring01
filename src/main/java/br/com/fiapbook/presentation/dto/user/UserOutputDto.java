2923c432bec531e3597fc1aba3e278312130c09f
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2023-09-17 18:18:03 -0300

    feat(user): DTO from user.

---
+++ src/main/java/br/com/fiapbook/presentation/dto/user/UserOutputDto.java
@@ -0,0 +1,22 @@
+package br.com.fiapbook.presentation.dto.user;
+
+import br.com.fiapbook.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+
+public record UserOutputDto(
+    String id,
+    String name,
+    String email
+) {
+
+  public UserOutputDto(User user) {
+    this(user.getId().toString(),
+        user.getName(),
+        user.getEmail());
+  }
+
+  public static Page<UserOutputDto> toPage(Page<User> usersPage) {
+    return usersPage.map(UserOutputDto::new);
+  }
+
+}
