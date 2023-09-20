2905d04939dfc3e3d4a74b72ca973be7ee5870ef
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2023-09-19 21:33:27 -0300

    feat(user): method to find user by email.

--- src/main/java/br/com/fiapbook/user/model/repository/UserRepository.java
+++ src/main/java/br/com/fiapbook/user/model/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package br.com.fiapbook.user.model.repository;
 
 import br.com.fiapbook.user.model.entity.User;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+  Optional<User> findByEmail(String email);
 
 }
--- src/main/java/br/com/fiapbook/user/model/service/UserService.java
+++ src/main/java/br/com/fiapbook/user/model/service/UserService.java
@@ -1,7 +1,8 @@
 package br.com.fiapbook.user.model.service;
 
-import br.com.fiapbook.user.model.repository.UserRepository;
 import br.com.fiapbook.user.model.entity.User;
+import br.com.fiapbook.user.model.repository.UserRepository;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -15,11 +16,15 @@ public class UserService {
     this.userRepository = userRepository;
   }
 
-  public User save(User user){
+  public User save(User user) {
     return userRepository.save(user);
   }
 
-  public Page<User> getAllUsersPaginated(Pageable pageable){
+  public Page<User> getAllUsersPaginated(Pageable pageable) {
     return userRepository.findAll(pageable);
+  }
+
+  public Optional<User> findByEmail(String email) {
+    return userRepository.findByEmail(email);
   }
 }
