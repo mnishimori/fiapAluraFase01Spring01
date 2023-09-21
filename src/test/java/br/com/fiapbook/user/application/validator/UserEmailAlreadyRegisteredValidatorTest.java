8bdbdf6e36ba8606d1309406995a3a83e30b2c23
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2023-09-21 14:31:10 -0300

    test(user): user email already registered validator.

---
+++ src/test/java/br/com/fiapbook/user/application/validator/UserEmailAlreadyRegisteredValidatorTest.java
@@ -0,0 +1,41 @@
+package br.com.fiapbook.user.application.validator;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import br.com.fiapbook.shared.exception.DuplicatedException;
+import br.com.fiapbook.shared.testData.user.UserTestData;
+import br.com.fiapbook.user.model.service.UserService;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UserEmailAlreadyRegisteredValidatorTest {
+
+  @Mock
+  private UserService userService;
+  @InjectMocks
+  private UserEmailAlreadyRegisteredValidator userEmailAlreadyRegisteredValidator;
+
+  @Test
+  void shouldValidateWhenUserEmailDoesNotExist() {
+    var user = UserTestData.getUserWithoutId();
+    Mockito.when(userService.findByEmail(user.getEmail())).thenReturn(Optional.empty());
+
+    assertDoesNotThrow(() -> userEmailAlreadyRegisteredValidator.validate(user.getEmail()));
+  }
+
+  @Test
+  void shouldThrowExceptionWhenUserAlreadyExists() {
+    var user = UserTestData.getUserWithoutId();
+    Mockito.when(userService.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+
+    assertThrows(DuplicatedException.class,
+        () -> userEmailAlreadyRegisteredValidator.validate(user.getEmail()));
+  }
+}
