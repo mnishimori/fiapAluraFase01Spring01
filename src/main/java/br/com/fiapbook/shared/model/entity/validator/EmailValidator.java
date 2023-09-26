c86e56342251f659b1f79bca5d7517b660197e32
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2023-09-26 15:25:28 -0300

    feat(email): email validator.

---
+++ src/main/java/br/com/fiapbook/shared/model/entity/validator/EmailValidator.java
@@ -0,0 +1,22 @@
+package br.com.fiapbook.shared.model.entity.validator;
+
+import br.com.fiapbook.shared.exception.ValidatorException;
+import br.com.fiapbook.user.model.messages.UserMessages;
+import java.util.regex.Pattern;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailValidator {
+
+  private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+      "[a-zA-Z0-9_+&*-]+)*@" +
+      "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+      "A-Z]{2,7}$";
+
+  public void validate(String email) {
+    var validEmail = Pattern.compile(EMAIL_REGEX).matcher(email).matches();
+    if (!validEmail) {
+      throw new ValidatorException(UserMessages.USER_EMAIL_INVALID.formatted(email));
+    }
+  }
+}
---
+++ src/test/java/br/com/fiapbook/shared/model/entity/validator/EmailValidatorTest.java
@@ -0,0 +1,32 @@
+package br.com.fiapbook.shared.model.entity.validator;
+
+import static br.com.fiapbook.shared.testData.user.UserTestData.DEFAULT_USER_EMAIL;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import br.com.fiapbook.shared.exception.ValidatorException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class EmailValidatorTest {
+
+  @Spy
+  private EmailValidator emailValidator;
+
+  @Test
+  void shouldValidateEmail() {
+    assertDoesNotThrow(() -> emailValidator.validate(DEFAULT_USER_EMAIL));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"email.domain.com", " email.domain.com", "@", "1", "email@domain",
+      "A@b@c@example.com", "a\"b(c)d,e:f;g<h>i[j\\k]l@example.com", "email @example.com"})
+  void shouldThrowExceptionWhenEmailIsInvalid(String email) {
+    assertThrows(ValidatorException.class, () -> emailValidator.validate(email));
+  }
+}
